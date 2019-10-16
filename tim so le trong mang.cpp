ae46dcb480531a5ba200c1662503f7b6e4596fa2
kieutienanh2k <56117896+kieutienanh2k@users.noreply.github.com>
2019-10-16 10:34:37 -0700

    btvn
    btvn

--- tim so le trong mang.cpp
+++ tim so le trong mang.cpp
@@ -3,7 +3,7 @@ int main ()
 {   int ary[20]; 
 	for (int i=0;i<20;i++)
 	{
-		printf ("nhap phan tu thu%d:\n",i);
+		printf ("nhap phan tu thu %d:\n",i);
     	scanf ("%d",&ary[i]);
 	 } 
 	 for (int i=0;i<20;i++)
